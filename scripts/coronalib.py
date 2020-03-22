c2ced1e11e3f8af601942cc31df7298b664835e6
tspr <t.sproll@gmail.com>
2020-03-22 20:58:56 +0100

    vereinheitlichung ic und hospitality

--- scripts/coronalib.py
+++ scripts/coronalib.py
@@ -12,7 +12,7 @@ statdef = {0: "not infected", 1: "immun or dead", 2: "infected",
            6: 'intensive'}
 
 
-def infection_profile(gmean=7.0, gsd=3.4, nday=21):
+def profiles(gmean=7.0, gsd=3.4, nday=28):
     """Calc the infections profile."""
     p = gmean**2/gsd**2
     b = gsd**2/gmean
@@ -20,7 +20,7 @@ def infection_profile(gmean=7.0, gsd=3.4, nday=21):
     y = gamma.cdf(x, a=p, scale=b)
     delay = np.zeros(nday+1)
     delay[1:(nday+1)] = y[1:(nday+1)] - y[0:nday]
-    return x, y, delay
+    return delay
 
 
 def makeprofile_plot():
@@ -63,7 +63,9 @@ def makeprofile_plot():
 
 def sim(age, gender, dr, r, gmean=7.0, gsd=3.4, nday=140, burnin=1000,
         cutdown=25000, cutr=None, prob_icu=0.05, mean_days_to_icu=12,
-        std_days_to_icu=3, mean_duration_icu=10, std_duration_icu=3):
+        std_days_to_icu=3, mean_duration_icu=10, std_duration_icu=3,
+        gmean_h=5.9, gsd_h=5.1, prob_h=0.1
+        ):
     """Simulate model.
 
     Parameters:
@@ -104,22 +106,20 @@ def sim(age, gender, dr, r, gmean=7.0, gsd=3.4, nday=140, burnin=1000,
     statesum = np.zeros(shape=(nstate, nday))
     statesum[:, 0] = np.bincount(state[0, :], minlength=nstate)
 
+
+
     # Precalculate profile infection
-    p = gmean**2/gsd**2
-    b = gsd**2/gmean
-    x = np.linspace(0, 28, num=29, dtype=("int"))
-    x = gamma.cdf(x, a=p, scale=b)
-    delay = x[1:29] - x[0:28]
-    delay = np.ascontiguousarray(delay[::-1])
-
-    # Precalculate time to icu
-    p = mean_days_to_icu**2/std_days_to_icu**2
-    b = std_days_to_icu**2/mean_days_to_icu
-    x = np.linspace(0, 28, num=29, dtype=("int"))
-    x = gamma.cdf(x, a=p, scale=b)
-    delay_icu = x[1:29] - x[0:28]
-    delay_icu = np.ascontiguousarray(delay_icu[::-1])
-
+    delay=profiles(gmean=gmean, gsd=gsd, nday=28)
+    
+    # Precalculate profile time to icu
+    delay_icu=profiles(gmean=mean_days_to_icu,gsd=std_days_to_icu, nday=28)
+    
+    #Precalculate profile time to hosp
+    delay_h=profiles(gmean=gmean_h,gsd=gsd_h, nday=28)
+    
+    #individual prop hospital
+    ind_prob_h = dr/np.mean(dr) * prob_h
+    print("Mean prob hospital:" + str(np.mean(ind_prob_h)))
     # individual prob icu
     ind_prob_icu = dr/np.mean(dr) * prob_icu
     print("Mean prob icu:" + str(np.mean(ind_prob_icu)))
@@ -139,6 +139,7 @@ def sim(age, gender, dr, r, gmean=7.0, gsd=3.4, nday=140, burnin=1000,
     firstdayinfected[state[0, :] == 2] = 0
 
     firstdayicu = np.full(shape=n, fill_value=1000, dtype="int")
+    firstdayinhospital = np.full(shape=n, fill_value=1000, dtype="int")
 
     # save the original r
     rstart = r
@@ -189,6 +190,20 @@ def sim(age, gender, dr, r, gmean=7.0, gsd=3.4, nday=140, burnin=1000,
         state[i, filt] = 1
         # release all people with more than 27 days on icu
         state[i, days_icu == 27] = 1
+
+        
+        # for infected cases calcualate the probality of hospital admission
+        days_infected_lim = np.clip(days_infected, 0, 27)
+        ph = delay_h[days_infected_lim] * ind_prob_h
+        rans = np.random.random(size=n)
+
+        # only those in state infected can go to hospital
+        filt = (rans < ph) & (state[i, ] == 2)
+        state[i, filt] = 5
+
+        # calculate the number of days in hospital
+        days_h = np.clip(i - firstdayinhospital, 0, 27)
+
 
         # infection probabilties by case
         pinf = r * newinf / n
@@ -346,132 +361,132 @@ def analysestate(state, title="Scenario", group=None, day0=0):
 
 
 
-def sim(age, gender, dr, r, gmean=7.0, gsd=3.4, nday=140, ni=500,
-        cutdown=25000, cutr=0.7,nh=10, gsd_h=5.1,gmean_h=5.9, p_h=1/10):
-    """Simulate model.
-
-    Parameters:
-    -----------
-    age : array of length n with the age of each individual
-    gender :  array of length n with the gender of each individual
-    dr :  array of length n with the daily mortality rate of each individual
-    r : array of lenght n with the averay indivial r
-    gmean : mean of the gamma distribution for the infections profile
-    gstd : std of the gamma distribution for the infections profile
-    nday : number of days to simulated
-    ni : number of infections to be assigned in the initial population
-    p_h: probability that someone needs to go to hospital after infection
-    nh= number of people initally in hospital
-    gmean_h= mean of gamma distribution for the hospitality time profile
-    gsd_h= std of gamma distribution for the hospitality time profile
-    
-    Returns:
-    --------
-    state : array shape (n,nday) with the state of each indivial on every day
-        0 : not infected
-        1 : immun
-        2.: infected but not identified
-        3.: infected and identified (not used yet)
-        4 : dead
-        5:  in hospital 
-    statesum : array of shape (5, nday) with the count of each individual per
-        days
-    infections : array of length nday with the number of infections
-    hospitalization: array of length nday with the number of people in hospital
-    """
-    # start configuation
-    n = len(age)
-    state = np.zeros(shape=(nday, n), dtype="int")
-    # set ni individuals to infected
-    state[0, np.random.choice(n, ni)] = 2
-    # set nh people to be in hospital
-    state[0, np.random.choice(n, nh)] = 6
-
-    
-    nstate = 7
-    statesum = np.zeros(shape=(nstate, nday))
-    statesum[:, 0] = np.bincount(state[0, :], minlength=nstate)
-    
-   
-    
-    # Precalculate profile
-    p = gmean**2/gsd**2
-    b = gsd**2/gmean
-    x = np.linspace(0, 28, num=29, dtype=("int"))
-    x = gamma.cdf(x, a=p, scale=b)
-    delay = x[1:29] - x[0:28]
-    delay = np.ascontiguousarray(delay[::-1])
-
-    
-
-    # Precalculate profile for hospitalzed people
-    p_h = gmean_h**2/gsd_h**2
-    b_h = gsd_h**2/gmean_h
-    x_h = np.linspace(0, 28, num=29, dtype=("int"))
-    x_h = gamma.cdf(x, a=p_h, scale=b_h)
-    delay_h = x_h[1:29] - x_h[0:28]
-    delay_h = np.ascontiguousarray(delay_h[::-1])
-
-
-    infections = np.zeros(shape=nday)
-    infections[0] = np.sum(state[0, :] == 2)
-    
-    hospitalzed = np.zeros(shape=nday)
-    hospitalzed[0] = np.sum(state[0, :] == 6)
- 
-    firstdayinfected = np.full(shape=n, fill_value=1000, dtype="int")
-    firstdayinfected[state[0, :] == 2] = 0
-    
-    firstdayinhospital = np.full(shape=n, fill_value=1000, dtype="int")
-    firstdayinhospital[state[0, :] == 6] = 0
-
-    for i in range(1, nday):
-        # set state to state day before
-        state[i, :] = state[i-1, :]
-
-        # New infections on day i
-        imin = max(0, i-28)
-        h = infections[imin: i]
-        newinf = np.sum(h*delay[-len(h):])
-        
-        #new people in hospital
-        h_h=hospitalzed[imin:i]
-        newhos=np.sum(h*delay_h[-len(h):])
-
-
-        # Generate randoms
-        rans = np.random.random(size=n)
-
-        # unconditional deaths
-        state[i, rans < dr] = 4
-
-        # Calculate the number of days infected
-        days_infected = i - firstdayinfected
-        # set all non dead cases with more than 27 days to status "immun"
-        state[i, (days_infected > 27) & (state[i, :] != 4)] = 1
-
-        # infection probabilties by case
-        pinf = r * newinf / n
-        # only not infected people can be infected
-        filt = (rans < pinf) & (state[i, ] == 0)
-        state[i, filt] = 2
-        firstdayinfected[filt] = i
-        # new infections
-        infections[i] = np.sum(filt)
-
-        #hosptitality probabilties by case * basic probability that someone goes to hospital if infected
-        phos=p_h*newhos/n
-        #only infected people can go to hospital
-        filt_h= (rans<phos) & (state[i, ]==2)
-        state[i, filt_h]=5
-  
-        hospitalzed[i]=np.sum(filt_h)
-
-        statesum[:, i] = np.bincount(state[i, :], minlength=nstate)
-        # if the number of infection exceeds cutdown r is devided by cutr
-        if statesum[2, i] > cutdown:
-            r = r / np.mean(r) * cutr
-        else:
-            pass
-
-    return state, statesum, infections,hospitalzed
+#def sim(age, gender, dr, r, gmean=7.0, gsd=3.4, nday=140, ni=500,
+#        cutdown=25000, cutr=0.7,nh=10, gsd_h=5.1,gmean_h=5.9, p_h=1/10):
+#    """Simulate model.
+#
+#    Parameters:
+#    -----------
+#    age : array of length n with the age of each individual
+#    gender :  array of length n with the gender of each individual
+#    dr :  array of length n with the daily mortality rate of each individual
+#    r : array of lenght n with the averay indivial r
+#    gmean : mean of the gamma distribution for the infections profile
+#    gstd : std of the gamma distribution for the infections profile
+#    nday : number of days to simulated
+#    ni : number of infections to be assigned in the initial population
+#    p_h: probability that someone needs to go to hospital after infection
+#    nh= number of people initally in hospital
+#    gmean_h= mean of gamma distribution for the hospitality time profile
+#    gsd_h= std of gamma distribution for the hospitality time profile
+#    
+#    Returns:
+#    --------
+#    state : array shape (n,nday) with the state of each indivial on every day
+#        0 : not infected
+#        1 : immun
+#        2.: infected but not identified
+#        3.: infected and identified (not used yet)
+#        4 : dead
+#        5:  in hospital 
+#    statesum : array of shape (5, nday) with the count of each individual per
+#        days
+#    infections : array of length nday with the number of infections
+#    hospitalization: array of length nday with the number of people in hospital
+#    """
+#    # start configuation
+#    n = len(age)
+#    state = np.zeros(shape=(nday, n), dtype="int")
+#    # set ni individuals to infected
+#    state[0, np.random.choice(n, ni)] = 2
+#    # set nh people to be in hospital
+#    state[0, np.random.choice(n, nh)] = 6
+#
+#    
+#    nstate = 7
+#    statesum = np.zeros(shape=(nstate, nday))
+#    statesum[:, 0] = np.bincount(state[0, :], minlength=nstate)
+#    
+#   
+#    
+#    # Precalculate profile
+#    p = gmean**2/gsd**2
+#    b = gsd**2/gmean
+#    x = np.linspace(0, 28, num=29, dtype=("int"))
+#    x = gamma.cdf(x, a=p, scale=b)
+#    delay = x[1:29] - x[0:28]
+#    delay = np.ascontiguousarray(delay[::-1])
+#
+#    
+#
+#    # Precalculate profile for hospitalzed people
+#    p_h = gmean_h**2/gsd_h**2
+#    b_h = gsd_h**2/gmean_h
+#    x_h = np.linspace(0, 28, num=29, dtype=("int"))
+#    x_h = gamma.cdf(x, a=p_h, scale=b_h)
+#    delay_h = x_h[1:29] - x_h[0:28]
+#    delay_h = np.ascontiguousarray(delay_h[::-1])
+#
+#
+#    infections = np.zeros(shape=nday)
+#    infections[0] = np.sum(state[0, :] == 2)
+#    
+#    hospitalzed = np.zeros(shape=nday)
+#    hospitalzed[0] = np.sum(state[0, :] == 6)
+# 
+#    firstdayinfected = np.full(shape=n, fill_value=1000, dtype="int")
+#    firstdayinfected[state[0, :] == 2] = 0
+#    
+#    firstdayinhospital = np.full(shape=n, fill_value=1000, dtype="int")
+#    firstdayinhospital[state[0, :] == 6] = 0
+#
+#    for i in range(1, nday):
+#        # set state to state day before
+#        state[i, :] = state[i-1, :]
+#
+#        # New infections on day i
+#        imin = max(0, i-28)
+#        h = infections[imin: i]
+#        newinf = np.sum(h*delay[-len(h):])
+#        
+#        #new people in hospital
+#        h_h=hospitalzed[imin:i]
+#        newhos=np.sum(h*delay_h[-len(h):])
+#
+#
+#        # Generate randoms
+#        rans = np.random.random(size=n)
+#
+#        # unconditional deaths
+#        state[i, rans < dr] = 4
+#
+#        # Calculate the number of days infected
+#        days_infected = i - firstdayinfected
+#        # set all non dead cases with more than 27 days to status "immun"
+#        state[i, (days_infected > 27) & (state[i, :] != 4)] = 1
+#
+#        # infection probabilties by case
+#        pinf = r * newinf / n
+#        # only not infected people can be infected
+#        filt = (rans < pinf) & (state[i, ] == 0)
+#        state[i, filt] = 2
+#        firstdayinfected[filt] = i
+#        # new infections
+#        infections[i] = np.sum(filt)
+#
+#        #hosptitality probabilties by case * basic probability that someone goes to hospital if infected
+#        phos=p_h*newhos/n
+#        #only infected people can go to hospital
+#        filt_h= (rans<phos) & (state[i, ]==2)
+#        state[i, filt_h]=5
+#  
+#        hospitalzed[i]=np.sum(filt_h)
+#
+#        statesum[:, i] = np.bincount(state[i, :], minlength=nstate)
+#        # if the number of infection exceeds cutdown r is devided by cutr
+#        if statesum[2, i] > cutdown:
+#            r = r / np.mean(r) * cutr
+#        else:
+#            pass
+#
+#    return state, statesum, infections,hospitalzed
